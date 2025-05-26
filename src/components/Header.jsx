6a8fd5884ded335092014da14739360f2faf0f1a
Adithya070707 <edunuri1234@gmail.com>
2025-05-26 13:36:48 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+export default function Header() {
+  return (
+    <div className="App-Header-Row">
+      <h1 style={{ backgroundColor: "orange", color: "black" }}>
+        React Lessons
+      </h1>
+      <Link to="/">App1</Link>-<Link to="/app2">App2</Link>-
+      <Link to="/app3">App3</Link>-
+      <Link to="/app4">App4</Link>-
+      <Link to="/app5">App5</Link>-
+      <Link to="/app6">App6</Link>-
+      <Link to="/app7">App7</Link>
+         <Link to="/app8">App8</Link>
+          <Link to="/app9">App9</Link>
+          <Link to="/app10">App10</Link>
+    </div>
+  );
+}
+
